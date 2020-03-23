a6a97bf3719eb482e1dd39f089bbbb0df4d3eef9
dangeredwolf <dangeredwolf@gmail.com>
2020-03-23 12:38:05 -0400

    Update LoginController.js

--- src/LoginController.js
+++ src/LoginController.js
@@ -1,8 +1,10 @@
+const {ipcRenderer} = require("electron");
 
 class LoginController {
     constructor(data) {
 		window.loginOpen = true;
-		alert("You need to reauthenticate manually.");
+		//alert("You need to reauthenticate manually.");
+		ipcRenderer.send("auth_twitter")
 		throw "Authentication required";
     }
 
